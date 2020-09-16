e406820e03aa2e330d7e03a0d887e4b50d7b46f5
nekithockey75 <nikita01051997@gmail.com>
2020-09-16 16:25:27 +0500

    Add project files.

---
+++ FileParser.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Prom_IT
+{
+    class FileParser
+    {
+        public static List<string> parse(string fileName)
+        {
+            List<string> words = new List<string>();
+
+            if (File.Exists(fileName))
+            {
+                if (GetEncoding(fileName) != Encoding.UTF8)
+                {
+                    throw new FileLoadException();
+                }
+                // Process file
+                FileStream fs = File.OpenRead(fileName);
+                StreamReader reader = new StreamReader(fs);
+
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    foreach (string word in line.Split(' '))
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+            }
+            else
+            {
+                throw new FileNotFoundException();
+            }
+
+            return words;
+        }
+        /// <summary
+
+        /// Get File's Encoding
+
+        /// </summary>
+        /// <param name="filename">The path to the file
+        private static Encoding GetEncoding(string filename)
+        {
+            // This is a direct quote from MSDN:  
+            // The CurrentEncoding value can be different after the first
+            // call to any Read method of StreamReader, since encoding
+            // autodetection is not done until the first call to a Read method.
+
+            using (var reader = new StreamReader(filename, Encoding.Default, true))
+            {
+                if (reader.Peek() >= 0) // you need this!
+                    reader.Read();
+
+                return reader.CurrentEncoding;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+
+namespace Prom_IT
+{
+    class Program
+    {
+        public class Options
+        {
+            [Option('c', "create", Required = false, HelpText = "<path> Create new dictionary.", SetName = "create")]
+            public string Create { get; set; }
+            [Option('u', "update", Required = false, HelpText = "<path> Update existing dictionary.", SetName = "update")]
+            public string Update { get; set; }
+            [Option('r', "remove", Required = false, HelpText = "Remove existing dictionary.", SetName = "remove")]
+            public bool Remove { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            // Parse arguments here
+            /*
+             * --create, -c - Create new dictionary
+             * --update, -u - Update existing dictionary
+             * --remove, -r - Remove existing dictionary
+             * --help, -h - Show help.
+             */
+            CommandLine.Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(run)
+                .WithNotParsed(error);
+        }
+        static void run(Options opt)
+        {
+            if (opt.Create.Length != 0)
+            {
+                FileParser.parse(opt.Create);
+            }
+            else if (opt.Update.Length != 0)
+            {
+                FileParser.parse(opt.Create);
+            }
+            else if (opt.Remove)
+            {
+                // Remove data from db here
+            }
+        }
+        static void error(IEnumerable<Error> errors)
+        {
+
+        }
+    }
+}
