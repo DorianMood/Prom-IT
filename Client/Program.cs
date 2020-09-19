2f2e9c258452f293101b91a5f562010df83a97b9
nekithockey75 <nikita01051997@gmail.com>
2020-09-19 17:36:38 +0500

    Restructured

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using CommandLine;
+using CommandLine.Text;
+
+namespace Client
+{
+    class Program
+    {
+        public class Options
+        {
+            [Value(0, Required = true, HelpText = "Host name or IP addres.")]
+            public string Host { get; set; }
+            [Value(1, Required = true, HelpText = "Host port.")]
+            public string Port { get; set; }
+        }
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(Run)
+                .WithNotParsed(err => DisplayHelp());
+        }
+        static void Run(Options opt)
+        {
+            Console.WriteLine("Starting client");
+
+            Console.WriteLine($"{opt.Host}:{opt.Port}");
+
+            /**
+             * 1. client.exe ip/hostname port
+             */
+        }
+        static void DisplayHelp()
+        {
+            Console.WriteLine("USAGE");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting server");
+            /**
+             * 1. Multiple clients.
+             * 2. TCP/IP server.
+             * 3. server.exe db_path port
+             * 4. Change db content on the fly from console:
+             * 4.1. create filename
+             * 4.2. update filename
+             * 4.3. remove
+             */
+        }
+    }
+}
